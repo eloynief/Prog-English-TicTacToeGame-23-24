575252b9ede5e932558e0a4cbc0d95f4056479ca
enieves <enieves@106-02.info2>
2024-02-29 13:34:38 +0100

    test

---
+++ src/game/Game.java
@@ -0,0 +1,26 @@
+package game;
+
+public class Game {
+	
+	//variable for the height of the table
+	final static int TABLE_HEIGHT=3;
+	
+	//variable for the position x
+	char posX;
+	//variable for the position y
+	char posY;
+	
+	//
+	
+	
+	
+	public static void () {
+		
+		//
+//		System.out.println();
+		
+		
+	}
+	
+	
+}
---
+++ src/game/Main.java
@@ -0,0 +1,17 @@
+package game;
+
+public class Main {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+
+}
